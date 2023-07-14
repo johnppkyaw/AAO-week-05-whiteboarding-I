73207dfa5834374473b3b5c0277ac7a75b8766c8
John <jjpyae@gmail.com>
2023-07-14 15:34:17 -0700

    completes evenSumArray

--- practice-for-week-05-whiteboarding1.js
+++ practice-for-week-05-whiteboarding1.js
@@ -1,6 +1,30 @@
 const log = console.log;
 
 
+//evenSumArray
+const evenSumArray = array => {
+  let resultArr = [];
+  for (const element of array) {
+    resultArr.push(evenSum(element));
+  }
+  return resultArr;
+}
+//evenSumArray helper function
+function evenSum(num) {
+  let sum = 0;
+  while(num > 0) {
+    if (num % 2 === 0) sum += num;
+    num--;
+  }
+  return sum;
+}
+//Time complexity: O(N x M)
+//Space complexity: O(N)
+log('evenSumArray test cases:')
+log(evenSumArray([6, 7, 5])) // => [ 12, 12, 6 ]
+log(evenSumArray([2, 8, 3, 5])) // => [ 2, 20, 2, 6 ]
+log('\n');
+
 
 //numsToWords
 const numsToWords = string => {
@@ -22,10 +46,13 @@ const numsToWords = string => {
   }
   return resultStr;
 }
+//Time Complexity: O(N)
+//Space Complexity: O(1)
+log('numsToWords test cases:')
 log(numsToWords('42')) // => 'FourTwo'
 log(numsToWords('123')) // => 'OneTwoThree'
 log(numsToWords('159598')) // => 'OneFiveNineFiveNineEight'
-
+log('\n')
 
 
 
@@ -41,6 +68,8 @@ const moreDotLessDash = string => {
 }
 //Time Complexity = O(N) based on # of char
 //Space Complexity = O(1)
+
+log('moreDotLessDash test cases:')
 log(moreDotLessDash('2-D arrays are fun. I think.'));  // => true
 log(moreDotLessDash('.-.-.'));  // => true
 log(moreDotLessDash('.-'));  // => false

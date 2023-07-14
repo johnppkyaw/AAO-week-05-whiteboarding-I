13922b6c4b3b740ad3b0ae88e5d9d575fcbdcc62
John <jjpyae@gmail.com>
2023-07-14 15:22:25 -0700

    Completes numsToWords and moreDotLessDash

--- practice-for-week-05-whiteboarding1.js
+++ practice-for-week-05-whiteboarding1.js
@@ -2,8 +2,34 @@ const log = console.log;
 
 
 
+//numsToWords
+const numsToWords = string => {
+  let resultStr = '';
+  const numsDict = {
+    '1' : 'One',
+    '2' : 'Two',
+    '3' : 'Three',
+    '4' : 'Four',
+    '5' : 'Five',
+    '6' : 'Six',
+    '7' : 'Seven',
+    '8' : 'Eight',
+    '9' : 'Nine',
+    '0' : 'Zero'
+  }
+  for (const char of string) {
+    resultStr += numsDict[char];
+  }
+  return resultStr;
+}
+log(numsToWords('42')) // => 'FourTwo'
+log(numsToWords('123')) // => 'OneTwoThree'
+log(numsToWords('159598')) // => 'OneFiveNineFiveNineEight'
 
 
+
+
+//moreDotLessDash
 const moreDotLessDash = string => {
   let dashCount = 0;
   let dotCount = 0;
@@ -14,7 +40,7 @@ const moreDotLessDash = string => {
   return dotCount > dashCount;
 }
 //Time Complexity = O(N) based on # of char
-//Space Complexity - O(1)
+//Space Complexity = O(1)
 log(moreDotLessDash('2-D arrays are fun. I think.'));  // => true
 log(moreDotLessDash('.-.-.'));  // => true
 log(moreDotLessDash('.-'));  // => false

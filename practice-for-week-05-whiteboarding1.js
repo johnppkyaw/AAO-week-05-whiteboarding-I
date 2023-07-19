7bd19b48c21983cc26d5531effa67658cc4cdd2e
John <jjpyae@gmail.com>
2023-07-19 10:48:07 -0700

    completes valueCounter

--- practice-for-week-05-whiteboarding1.js
+++ practice-for-week-05-whiteboarding1.js
@@ -1,8 +1,27 @@
 const log = console.log;
 
 //valueCounter
-const valueCounter =
+const valueCounter = (obj, value) => {
+  let count = 0;
+  for (let key in obj) {
+    if (obj[key] === value) {
+      count++
+    }
+  }
+  return count;
+}
+//Time: O(N)
+//Space: O(1)
+log('valueCounter test cases:')
+const obj1 = { 1: 'Anne', 2: 'Alvin', 3: 'Anne', 4: 'Anne' }
+log(valueCounter(obj1, 'Anne'))  // => 3
 
+const obj2 = { Anne: 50, Alvin: 1, JJ: 100, Roman: 100 }
+log(valueCounter(obj2, 88))  // => 0
+
+const pairs = { Anne: 'Roman', Alvin: 'Roman', JJ: 'Anne', Roman: 'Anne' }
+log(valueCounter(pairs, 'Roman'))  // => 2
+log('\n');
 
 //elementCount
 const elementCount = arr => {
